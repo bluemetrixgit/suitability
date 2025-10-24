bae9261ef24927966a497d287d3b43aab213ea58
bluemetrixgit <165940023+bluemetrixgit@users.noreply.github.com>
2025-10-24 10:53:50 -0300

    Refactor email configuration to use direct access

--- suitability_main.py
+++ suitability_main.py
@@ -63,8 +63,8 @@ st.write("Preencha os campos abaixo para realizar o cadastro do Suitability")
 
 # --------------------------- E-MAIL (envio automático) ---------------------------
 # Configuração de e-mail (envio sempre do mesmo endereço)
-EMAIL_USER = st.secrets.get("EMAIL_USER", EMAIL_USER)
-EMAIL_PASSWORD = st.secrets.get("EMAIL_PASSWORD", EMAIL_PASSWORD)
+EMAIL_USER = st.secrets["EMAIL_USER"]
+EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
 EMAIL_FROM_NAME = "Middle Office Bluemetrix"
 EMAIL_TO = "backoffice@bluemetrix.com.br"
 
